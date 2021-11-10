e9e6cfee45a59bb68079a8c83a1c83744ac09e96
Dan Possehl <danielbpossehl@gmail.com>
2021-11-10 11:18:50 -0700

    created basic structure for context

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
 import client from "shopify-buy";
 
+const ShopContext = React.createContext;
+
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-  domain: "your-shop-name.myshopify.com",
-  storefrontAccessToken: "your-storefront-access-token",
+  domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
 });
+
+// Add STATE which will handle all of our data
+
+state = {
+  product: {},
+  //products is all the products we want to recieve
+  products: [],
+  //checkout will contain our checkout data
+  checkout: {},
+  isCartOpen: false,
+  isMenuOpen: false,
+};
+
+createCheckout = async () => {};
+
+fetchCheckout = async () => {};
+
+addItemToCheckout = async () => {};
+
+removeLineItem = async (lineItemIdsToRemove) => {};
+
+fetchAllProducts = async () => {};
+
+fetchProductWithHandle = async (handle) => {};
+
+closeCart = () => {};
+
+openCart = () => {};
+
+closeMenu = () => {};
+
+openMenu = () => {};
 
 export class shopProvider extends Component {
   render() {
-    return <div></div>;
+    return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>;
   }
 }
 
