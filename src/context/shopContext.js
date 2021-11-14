2ac626e4703fb93b809ecc42db03fdffd50d3baf
Dan Possehl <danielbpossehl@gmail.com>
2021-11-14 11:59:11 -0700

    fixed issues

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import client from "shopify-buy";
+import Client from "shopify-buy";
 
-const ShopContext = React.createContext;
+const ShopContext = React.createContext();
 
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
@@ -10,53 +10,58 @@ const client = Client.buildClient({
 });
 
 // Add STATE which will handle all of our data
+class ShopProvider extends Component {
+  state = {
+    product: {},
+    //products is all the products we want to recieve
+    products: [],
+    //checkout will contain our checkout data
+    checkout: {},
+    isCartOpen: false,
+    isMenuOpen: false,
+  };
 
-state = {
-  product: {},
-  //products is all the products we want to recieve
-  products: [],
-  //checkout will contain our checkout data
-  checkout: {},
-  isCartOpen: false,
-  isMenuOpen: false,
-};
+  createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    //using local storage/ its called checkout-id and we are saving checkout.id
+    localStorage.setItem("checkout-id", checkout.id);
+    this.setState({ checkout: checkout });
+  };
 
-createCheckout = async () => {
-  const checkout = await client.checkout.create();
-  //using local storage/ its called checkout-id and we are saving checkout.id
-  localstorage.setItem("checkout-id", checkout.id);
-  this.setState({ checkout: checkout });
-};
+  fetchCheckout = async () => {};
 
-fetchCheckout = async () => {};
+  addItemToCheckout = async () => {};
 
-addItemToCheckout = async () => {};
+  removeLineItem = async (lineItemIdsToRemove) => {};
 
-removeLineItem = async (lineItemIdsToRemove) => {};
+  fetchAllProducts = async () => {
+    // Fetch all products in your shop
+    const products = await client.product.fetchAll();
+    this.setState({ products: products });
+  };
 
-fetchAllProducts = async () => {
-  // Fetch all products in your shop
-  const products = await client.product.fetchAll();
-  this.setState({ products: products });
-};
+  fetchProductWithHandle = async (handle) => {
+    const product = await client.product.fetchByHandle(handle);
+    this.setState({ product: product });
+  };
 
-fetchProductWithHandle = async (handle) => {
-  const product = await client.product.fetchByHandle(handle);
-  this.setState({ product: product });
-};
+  closeCart = () => {};
 
-closeCart = () => {};
+  openCart = () => {};
 
-openCart = () => {};
+  closeMenu = () => {};
 
-closeMenu = () => {};
+  openMenu = () => {};
 
-openMenu = () => {};
-
-export class shopProvider extends Component {
   render() {
+    console.log(this.state.checkout);
+
     return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>;
   }
 }
 
-export default shopProvider;
+const ShopConsumer = ShopContext.Consumer;
+
+export { ShopContext, ShopConsumer };
+
+export default ShopProvider;
