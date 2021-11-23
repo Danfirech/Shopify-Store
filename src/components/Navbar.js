0b4774122277621fa42719a15b96b26c5352bbe8
Dan Possehl <danielbpossehl@gmail.com>
2021-11-23 11:03:27 -0700

    added link to logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ShopContext } from "../context/shopContext";
 import { Flex, Text, Icon, Image } from "@chakra-ui/react";
 import { MdMenu, MdShoppingCart } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { openCart, openMenu, checkout } = useContext(ShopContext);
@@ -16,11 +17,13 @@ const Navbar = () => {
       <Icon fill="white" as={MdMenu} w={30} h={30} cursor="pointer">
         Menu
       </Icon>
-      <Image
-        src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540"
-        w={100}
-        h={100}
-      ></Image>
+      <Link to="/">
+        <Image
+          src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540"
+          w={100}
+          h={100}
+        />
+      </Link>
       <Icon
         fill="white"
         as={MdShoppingCart}
