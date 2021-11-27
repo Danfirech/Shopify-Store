74063109697609a025ad3cec0659b6e29090142e
Dan Possehl <danielbpossehl@gmail.com>
2021-11-27 13:05:22 -0500

    added hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import { Box, Button, Text, Image, Center } from "@chakra-ui/react";
 
 const Hero = () => {
   return (
-    <Box backgroundColor="#FFA8E2" w="100%" position="realive" h="70vh">
+    <Box backgroundColor="#FFA8E2" w="100%" position="relative" h="70vh">
       <Image
         src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Bath_Bomb_-_Product-4_-_nobg_1.png?v=1610055851.jpg"
         h="100%"
@@ -11,6 +11,29 @@ const Hero = () => {
         objectFite="contain"
         objectPosition={["top", "center"]}
       />
+      <Text
+        position="absolute"
+        bottom="18%"
+        w="100%"
+        textAlign="center"
+        color="white"
+        fontWeight="bold"
+        fontSize="4rem"
+      >
+        Introducing Bath Bombs
+      </Text>
+      <Center>
+        <Button
+          w="10rem"
+          backgroundColor="#ff38BD"
+          color="white"
+          _hover={{ opacity: "70%" }}
+          position="absolute"
+          bottom="10%"
+        >
+          Shop Now
+        </Button>
+      </Center>
     </Box>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { ShopContext } from "../context/shopContext";
 import { Link } from "react-router-dom";
 import { Box, Grid, Text, Image } from "@chakra-ui/react";
+import Hero from "../components/Hero";
 
 const Home = () => {
   const { fetchAllProducts, products } = useContext(ShopContext);
