19ba47c4785d078673924b1f52e51bd695a06541
Dan Possehl <danielbpossehl@gmail.com>
2021-11-28 13:20:37 -0500

    added RichText component

---
+++ src/components/RichText.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Heading, Text, Center } from "@chakra-ui/react";
+
+const RichText = ({ heading, text }) => {
+  return (
+    <Box p="4rem">
+      <Center display="flex" flexDir="column" textAlign="center">
+        <Heading py="4rem">{heading && heading}</Heading>
+        <Text>{text && text}</Text>
+      </Center>
+    </Box>
+  );
+};
+
+export default RichText;
